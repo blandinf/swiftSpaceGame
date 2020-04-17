dedc54a461083e341b57bb878462e66d4f77292e
Florian Blandin <pro.blandinf@gmail.com>
2020-04-17 10:24:40 +0200

    add assets and implement scene and player

--- SpaceGame/GameScene.swift
+++ SpaceGame/GameScene.swift
@@ -9,8 +9,22 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let player = SKSpriteNode(imageNamed: "player")
     
     override func didMove(to view: SKView) {
-        
+        // space scene
+        if let particles = SKEmitterNode(fileNamed: "Starfield") {
+            particles.position = CGPoint(x: 1080, y: 0)
+            particles.advanceSimulationTime(60)
+            particles.zPosition = -1
+            addChild(particles)
+        }
+            
+        //player first position
+        player.name = "player"
+        player.position.x = frame.minX + 75
+        player.zPosition = 1
+        addChild(player)
+    
     }
 }
