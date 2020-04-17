93273d903b13d6367b7c569b02995c5a7fd66047
Florian Blandin <pro.blandinf@gmail.com>
2020-04-17 11:02:56 +0200

    add enemies sprite, collision and movements

---
+++ SpaceGame/EnemyNode.swift
@@ -0,0 +1,51 @@
+//
+//  EnemyNode.swift
+//  SpaceGame
+//
+//  Created by Florian on 17/04/2020.
+//  Copyright © 2020 blandinf. All rights reserved.
+//
+
+import SpriteKit
+
+class EnemyNode: SKSpriteNode {
+    var type: EnemyType
+    var lastFireTime: Double = 0
+    var shields: Int
+    
+    init(type: EnemyType, startPosition: CGPoint, xOffset: CGFloat, moveStraight: Bool) {
+        self.type = type
+        shields = type.shields
+        
+        let texture = SKTexture(imageNamed: type.name)
+        super.init(texture: texture, color: .white, size: texture.size())
+        
+        physicsBody = SKPhysicsBody(texture: texture, size: texture.size())
+        physicsBody?.categoryBitMask = CollisionType.enemy.rawValue
+        physicsBody?.collisionBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
+        physicsBody?.contactTestBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
+        name = "enemy"
+        position = CGPoint(x: startPosition.x + xOffset, y: startPosition.y)
+        
+        configureMovement(moveStraight)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("NO")
+    }
+    
+    func configureMovement(_ moveStraight: Bool) {
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        
+        if moveStraight {
+            path.addLine(to: CGPoint(x: -10000, y: 0))
+        } else {
+            path.addCurve(to: CGPoint(x: -3500, y: 0), controlPoint1: CGPoint(x: 0, y: -position.y * 4), controlPoint2: CGPoint(x: -1000, y: -position.y))
+        }
+        
+        let movement = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: type.speed)
+        let sequence = SKAction.sequence([movement, .removeFromParent()])
+        run(sequence)
+    }
+}
